1bc26c772c6845af2752dffb14ff8a880a164a9c
iDKo34 <83545548+iDKo34@users.noreply.github.com>
2023-10-26 12:26:44 +0200

    bouton réinitialiser

---
+++ JS/b_reini.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Fonction pour ouvrir la fenêtre contextuelle
+    function openModal() {
+        const modal = document.getElementById("passwordResetModal");
+        modal.style.display = "block";
+    }
+
+    // Fonction pour fermer la fenêtre contextuelle
+    function closeModal() {
+        const modal = document.getElementById("passwordResetModal");
+        modal.style.display = "none";
+    }
+
+    // Fermer la fenêtre contextuelle en cliquant sur la bouton "X"
+    document.getElementById("closeModal").addEventListener("click", closeModal);
+
+    // Fermer la fenêtre contextuelle en cliquant en dehors de la fenêtre
+    window.addEventListener("click", function (event) {
+        const modal = document.getElementById("passwordResetModal");
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    // Afficher la fenêtre contextuelle lorsque le bouton est cliqué
+    document.querySelector(".reset-password-button").addEventListener("click", openModal);
+
+    // Gérer la soumission du formulaire de réinitialisation du mot de passe
+    document.getElementById("passwordResetForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+        // Logique de réinitialisation du mot de passe ici
+        closeModal();
+    });
+});
