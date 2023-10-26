3e1247f87144e9c9ae4f635a41487e4ccdf52ad1
LightBeam <Guarrow@hotmail.fr>
2023-10-26 12:46:33 +0200

    page connexion et oubli de mdp

---
+++ traitement_connexion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Traitement de la Connexion</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+
+        #confirmation {
+            background-color: #ffffff;
+            border-radius: 5px;
+            width: 300px;
+            margin: 100px auto;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+</head>
+<body>
+
+<div id="confirmation">
+    <h1>Connexion Réussie</h1>
+    <p>Votre connexion a été effectuée avec succès.</p>
+</div>
+
+</body>
+</html>
