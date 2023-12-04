8d480d9dd0549cab1bbc8f91a7148f052272763b
iDKo34 <83545548+iDKo34@users.noreply.github.com>
2023-12-04 10:54:20 +0100

    page de connexion

--- JS/connexion.js
+++ JS/connexion.js
@@ -1,58 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Déplacez la déclaration de usersDatabase à l'extérieur de la fonction checkLogin
-    const usersDatabase = [
-        {
-            id: 1,
-            firstName: 'John',
-            lastName: 'Doe',
-            username: 'john_doe',
-            password: 'motdepasse1',
-            profileImage: 'path/to/image1.jpg',
-            orders: [
-                { orderId: 101, date: '2023-11-01', items: ['Spaghetti', 'Penne'] },
-                { orderId: 102, date: '2023-11-05', items: ['Fusilli'] }
-            ]
-        },
-        {
-            id: 2,
-            firstName: 'Jane',
-            lastName: 'Smith',
-            username: 'jane_smith',
-            password: 'motdepasse2',
-            profileImage: 'path/to/image2.jpg',
-            orders: [
-                { orderId: 103, date: '2023-11-02', items: ['Penne', 'Fusilli'] }
-            ]
-        }
-    ];
+// Simulated user database
+const users = [
+    { firstName: "John", lastName: "Doe", username: "john_doe", password: "password123" },
+    { firstName: "Jane", lastName: "Smith", username: "jane_smith", password: "securepass" }
+];
 
-    function checkLogin() {
-        const username = document.getElementById('login-box-username').value;
-        const password = document.getElementById('login-box-password').value;
+function login() {
+    const usernameInput = document.getElementById("username").value;
+    const passwordInput = document.getElementById("password").value;
+    const errorMessageElement = document.getElementById("errorMessage");
+    const successMessageElement = document.getElementById("successMessage");
 
-        // Utilisez la variable usersDatabase définie à l'extérieur de cette fonction
-        const userExists = checkUserExists(username, password, usersDatabase);
+    const user = users.find(u => u.username === usernameInput && u.password === passwordInput);
 
-        if (userExists) {
-            // Affichez le message de connexion réussie
-            document.getElementById('login-success-message').innerText = 'Connexion réussie ! Redirection vers l\'espace client...';
-
-            // Redirigez vers la page de l'espace client ou effectuez d'autres actions nécessaires
-            setTimeout(function () {
-                window.location.href = 'client.html';
-            }, 2000);  // Redirection après 2 secondes (ajustez le délai selon vos besoins)
-        } else {
-            alert('Nom d\'utilisateur ou mot de passe incorrect.');
-        }
+    if (user) {
+        // Successful login
+        errorMessageElement.textContent = "";
+        successMessageElement.textContent = `Welcome, ${user.firstName} ${user.lastName}!`;
+    } else {
+        // Failed login
+        errorMessageElement.textContent = "Invalid username or password. Please try again.";
+        successMessageElement.textContent = "";
     }
-
-    function checkUserExists(username, password, usersDatabase) {
-        // Utilisez la variable usersDatabase fournie en paramètre
-        for (const user of usersDatabase) {
-            if (user.username === username && user.password === password) {
-                return true;
-            }
-        }
-        return false;
-    }
-});
+}
